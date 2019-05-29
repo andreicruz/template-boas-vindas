8bfaf5451af395f5799baf051437fcab18a38cec
Andrei Dorneles Cruz <andrei.cruz@ibm.com>
2019-05-29 19:28:16 -0300

    alterado metodo de adicionar carta

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 import logo from './sicredi.png';
 import logoIBM from './IBM.png';
 import henrique from './henrique.jpg';
 import './teste.css';
 import html2canvas from 'html2canvas';
 
-class Foo extends Component {
-  handleClick() {
-	alert('Click happened');
-	html2canvas(document.querySelector(".app")).then(canvas => {
-		document.body.appendChild(canvas)
-	});
-  }
-  render() {
-    return <button onClick={this.handleClick}>Click Me</button>;
-  }
-}
-
-
-function App() {
-  return (
-    <div className="container">
-		<div className="app">
-			<div className="cabecalho">
-				<h1>Bem Vindo</h1>
-			</div>
-			<div className="logo">
-				<div className="sicredi"><img src={logo} alt="Logo Sicredi" /></div>
-				<div className="ibm"><img src={logoIBM} alt="Logo Sicredi" /></div>
-			</div>
-			<div className="carta carta--branca">
+class AdicionaCarta extends Component {
+ 	montaCarta() {
+		const element = (
+			<div className="carta carta--azul">
 				<div className="informacoes">
 					<div className="nome">
 						<input type="text" className="form-control form-control--azul" id="nomeFuncionario" placeholder="Nome Completo"/>
@@ -47,8 +27,33 @@ function App() {
 				<div className="foto">
 					<div className="perfil"><img src={henrique} alt="Foto" /></div>
 				</div>
+			</div>	
+		);
+		ReactDOM.render(element, document.getElementById('template'));
+
+		// html2canvas(document.querySelector(".app")).then(canvas => {
+		// 	document.body.appendChild(canvas)
+		// });
+	  }
+	  
+  	render() {
+		return <button onClick={this.montaCarta}>Click Me</button>
+  	}
+}
+
+function App() {
+  return (
+    <div className="container">
+		<div className="app" id="aplicacao">
+			<div className="cabecalho">
+				<h1>Bem Vindo</h1>
 			</div>
-			<Foo/>
+			<div className="logo">
+				<div className="sicredi"><img src={logo} alt="Logo Sicredi" /></div>
+				<div className="ibm"><img src={logoIBM} alt="Logo Sicredi" /></div>
+			</div>
+			<div id="template"></div>
+			<AdicionaCarta/>
 		</div>
     </div>
   );
